c314edbd8833abd75292aee69f7710214997f9d7
Hejie Cui <cuihejie331771@gmail.com>
2025-11-06 19:42:06 -0800

    upload heatmap code

--- scripts/plots/1_create_heatmaps.sh
+++ scripts/plots/1_create_heatmaps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-cd "$SCRIPT_DIR/$DIR" || { echo "Failed to change directory"; exit 1; }
+cd "$SCRIPT_DIR" || { echo "Failed to change directory"; exit 1; }
 
 # Construct the log file name
 LOG_FILE="../../logs/category_heatmap_$(date '+%Y-%m-%d_%H-%M-%S').log"
@@ -14,13 +14,19 @@ mkdir ../../plots
 
 python3 ../../src/medhelm/plots/1_heatmap.py \
     --leaderboard_path ../../data/leaderboard.csv \
+    --output_path_image ../../plots/heatmap.png \
+    --output_path_text ../../data/stats/heatmap_benchmark_analysis.txt
 
 # Category heatmap
 
 python3 ../../src/medhelm/plots/1_heatmap.py \
     --leaderboard_path ../../data/leaderboard.csv \
-    --aggregated Category
+    --aggregated Category \
+    --output_path_image ../../plots/heatmap.png \
+    --output_path_text ../../data/stats/heatmap_category_analysis.txt
 
 python3 ../../src/medhelm/plots/1_heatmap.py \
     --leaderboard_path ../../data/leaderboard.csv \
-    --aggregated Subcategory
+    --aggregated Subcategory \
+    --output_path_image ../../plots/heatmap.png \
+    --output_path_text ../../data/stats/heatmap_subcategory_analysis.txt
